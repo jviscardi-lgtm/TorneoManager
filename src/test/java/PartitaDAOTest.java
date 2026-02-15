4680c60898c1dd7d386f7cd1be91da41440e66ef
jviscardi-lgtm <j.viscardi@studenti.unisa.it>
2026-02-15 23:24:54 +0100

    Update PartitaDAOTest.java

--- src/test/java/PartitaDAOTest.java
+++ src/test/java/PartitaDAOTest.java
@@ -10,13 +10,18 @@ class PartitaDAOTest {
         PartitaDAO dao = new PartitaDAO();
 
       
-        int idPartitaReale = 1;
-        int golCasa = 5;
-        int golOspite = 5;
+        int idPartitaReale = 5;
+        int golCasa = 2;
+        int golOspite = 1;
 
         // Eseguiamo l'update
         assertDoesNotThrow(() -> dao.doUpdateRisultato(idPartitaReale, golCasa, golOspite));
-
+        Partita pAggiornata = dao.doRetrieveById(idPartitaReale);
+        assertNotNull(pAggiornata, "La partita deve esistere nel DB");
+        assertEquals(2, pAggiornata.getGolCasa(), "I gol in casa devono essere aggiornati a 2");
+        assertEquals(1, pAggiornata.getGolOspite(), "I gol in trasferta devono essere aggiornati a 1");
+        assertTrue(pAggiornata.isGiocata(), "Lo stato della partita deve essere 'giocata=true'");
+    
     }
 
     // TC_U_04.2: ID Partita Inesistente
@@ -24,7 +29,7 @@ class PartitaDAOTest {
     void testAggiornamentoPartitaInesistente() {
         PartitaDAO dao = new PartitaDAO();
 
-        int idPartitaFinta = 999999; // Un ID che sicuramente non c'è
+        int idPartitaFinta = 999; // Un ID che sicuramente non c'è
 
         // Il metodo non deve esplodere, semplicemente non aggiornerà nulla
         assertDoesNotThrow(() -> dao.doUpdateRisultato(idPartitaFinta, 1, 1));
