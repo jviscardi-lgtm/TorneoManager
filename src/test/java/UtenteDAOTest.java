23abdd87c67b50a16bb1ca3416bbbad32478e4b0
jviscardi-lgtm <j.viscardi@studenti.unisa.it>
2026-02-15 23:17:33 +0100

    Update UtenteDAOTest.java

--- src/test/java/UtenteDAOTest.java
+++ src/test/java/UtenteDAOTest.java
@@ -7,8 +7,8 @@ class UtenteDAOTest {
     void testLoginCorretto() {
         UtenteDAO dao = new UtenteDAO();
 
-        String emailEsistente = "admin@torneomanager.it";
-        String passwordCorretta = "admin";
+        String emailEsistente = "arbitro@arbitro.it";
+        String passwordCorretta = "arbitro";
 
         Utente u = dao.doRetrieveByEmailPassword(emailEsistente, passwordCorretta);
 
@@ -22,7 +22,7 @@ class UtenteDAOTest {
         UtenteDAO dao = new UtenteDAO();
 
 
-        String emailEsistente = "admin@torneomanager.it";
+        String emailEsistente = "arbitro@fischietto.it";
         String passwordErrata = "passwordSbagliata123";
 
         Utente u = dao.doRetrieveByEmailPassword(emailEsistente, passwordErrata);
